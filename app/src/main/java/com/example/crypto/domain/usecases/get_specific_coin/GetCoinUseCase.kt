af5d7888d26e3598e0720917d38769d3ef972727
Utkarsh Saxena <utkarshsaxena900@gmail.com>
2023-08-26 20:05:31 +0530

    Usecases Impl

---
+++ app/src/main/java/com/example/crypto/domain/usecases/get_specific_coin/GetCoinUseCase.kt
@@ -0,0 +1,26 @@
+package com.example.crypto.domain.usecases.get_specific_coin
+
+import com.example.crypto.common.Resource
+import com.example.crypto.data.remote.dto.toCoinDetail
+import com.example.crypto.domain.model.CoinDetail
+import com.example.crypto.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+    operator fun invoke(coinId: String): Flow<Resource<CoinDetail>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coin = repository.getCoinById(coinId).toCoinDetail()
+            emit(Resource.Success(coin))
+        } catch (e: Exception) {
+            emit(Resource.Error(e.localizedMessage ?: "Unexpected Error Occurred"))
+        } catch (e: IOException) {
+            emit(Resource.Error("Couldn't reach Server"))
+        }
+    }
+}
