75798b661e123ae075079249eeabddbf4c99318f
Utkarsh Saxena <utkarshsaxena900@gmail.com>
2023-08-27 15:42:38 +0530

    bug fix

--- app/src/main/java/com/example/crypto/presentation/coin_detail/CoinDetailScreen.kt
+++ app/src/main/java/com/example/crypto/presentation/coin_detail/CoinDetailScreen.kt
@@ -22,10 +22,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.crypto.presentation.Screen
 import com.example.crypto.presentation.coin_detail.components.CoinTag
 import com.example.crypto.presentation.coin_detail.components.TeamListItem
 import com.example.crypto.presentation.coin_list.components.CoinDetailViewModel
